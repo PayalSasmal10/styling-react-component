e633888d22de1e3922b50488ded55b014a1cad2d
Payal Sasmal <sasmalpayal@gmail.com>
2022-11-19 17:58:36 +0530

    media query added

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -13,6 +13,7 @@ import styled from 'styled-components';
 // };
 
 const Button = styled.button`
+  width: 100%;
   font: inherit;
   padding: 0.5rem 1.5rem;
   border: 1px solid #8b005d;
@@ -20,6 +21,10 @@ const Button = styled.button`
   background: #8b005d;
   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
   cursor: pointer;
+
+  @media (min-width: 768px){
+    width: auto;
+  }
 
   &:focus {
   outline: none;
